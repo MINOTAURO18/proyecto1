7f456d5ef6398f5cb41e617258ae794b7b93ccef
alexander muñoz <alexander12345my@gmail.com>
2023-08-11 16:04:41 -0500

    final

--- src/views/about/About.jsx
+++ src/views/about/About.jsx
@@ -1,6 +1,6 @@
 const About = () => {
     return(
-        <div className="w-full h-screen flex justify-center items-center">
+        <div className="p-7 w-full h-screen flex justify-center items-center">
            <h1 className="text-4xl">Proyecto practica: Hooks de react - Tailwind - CSS</h1>
         </div>
     )
